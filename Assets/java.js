a7e1a7ba8081f73a3db7f3f36b7a9a89f22604b1
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-11-26 14:28:23 -0500

    restructured the html and started adding fucntions in the script. can currently search a city and retrieve that cities weather

--- Assets/java.js
+++ Assets/java.js
@@ -1,1 +1,31 @@
-var APIKey = "fc1640bed3215e2b224eefde45f29f61";
+const APIKey = "fc1640bed3215e2b224eefde45f29f61";
+var searchLocation = document.querySelector("#search-btn");
+var userInput = document.querySelector("#user-input");
+var currentLocation = document.querySelector("#location-btn");
+var cityArrList = JSON.parse(localStorage.getItem("cities")) || [];
+
+
+searchLocation.addEventListener("click", () => {
+    var cityInput = userInput.value.trim().toLowerCase();
+
+
+    searchCity(cityInput, APIKey);
+    // getCityCoord(city, APIKey);
+})
+
+function searchCity(city, APIKey) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
+
+    fetch(queryURL)
+    .then(response => response.json())
+    .then(data => {
+        var { name: cityName, main: {temp: currentTemp, humidity: currentHumidity}, wind: {speed: currentWindSpeed}, weather: [{ icon: currentWeatherIcon}] } = data;
+
+        document.getElementById("cityName").textContent = cityName;
+        document.getElementById("currentTemp").textContent = `Temp: ${currentTemp}°F`;
+        document.getElementById("currentHumidity").textContent = `Humidity: ${currentHumidity}%`;
+        document.getElementById("currentWindSpeed").textContent = `Wind Speed: ${currentWindSpeed} MPH`;
+        document.getElementById("currentWeatherIcon").src = `http://openweathermap.org/img/w/${currentWeatherIcon}.png`;
+    })
+    .catch(error => console.error("Error", error));
+}
